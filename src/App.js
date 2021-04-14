3882bfe2065871539f8554571dacf19547e9ef0f
himzei <himzei@gmail.com>
2021-04-14 10:19:06 +0900

    Link Apply

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import MainPage from "./main";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import UploadPage from "./upload";
+import ProductPage from "./product";
 
 function App() {
   return (
     <div>
-      <MainPage />
+      <Switch>
+        <Route exact={true} path="/">
+          <MainPage />
+        </Route>
+        <Route exact={true} path="/upload">
+          <UploadPage />
+        </Route>
+        <Route exact={true} path="/products/:id">
+          <ProductPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
